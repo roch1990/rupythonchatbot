8abaf4d882b2e3f35e4621edb9236fd438ec50fb
roch1990 <eugene.gildin@yandex.ru>
2017-05-18 21:22:40 +0500

    News posting added

--- app/src/bot.py
+++ app/src/bot.py
@@ -77,15 +77,19 @@ def user_help(message):
 @bot.message_handler(content_types=["text"])
 def user_help(message):
     weather_flag = False
+    news_flag = False
     sys.stdout.write('{0}\tText method call\n'.format(message.chat.id))
     # Режем список  слов
     keywords_list = tuple(message.text.split(' '))
+    
+    if message.chat.id == config.ighar_chat_id :
+        bot.send_message(config.python_ru_chat_id, message.text)
     
     # Пробегаемся по всем значениям в словаре хештегов и подбираем релевантный хэштег
     for key in config.hashtag:
         for item in config.hashtag[key]:
             for word in keywords_list:
-                # Не забываем занить все как ладу приору
+                # Не забываем занизить все как ладу приору
                 if word.lower() == item:
                     bot.send_message(message.chat.id, key)
 
@@ -95,6 +99,9 @@ def user_help(message):
         # Если есть чето по погоде - ставим флаг готовности ее выдать
         if word.lower() in config.weather_key:
             weather_flag = True
+        
+        elif word.lower() in config.news_key:
+            news_flag = True
         
         # Если есть флаг погоды и выставлен город - парсим погоду с Рамблера и выдаем   
         elif (weather_flag == True) and (word.lower() in config.weather_hash):
@@ -119,7 +126,29 @@ def user_help(message):
             bot.send_message(message.chat.id,
                              'На райончике: ' +
                              str(datetime.datetime.now()))
-              
+        
+        # Новости с хабра питоновские
+        elif (news_flag == True):
+            # Для каждого ключа из хэша конфиговского
+            for key in config.habrahabr_rubik:
+                # Если слово из сообщения есть в значениях ключа хэша из конфига
+                if word.lower() in config.habrahabr_rubik[key]:
+                    # Берем релевантный url
+                    habr_url = config.habrahabr_url[key]
+                    # Парсим страничку на первые попавшиеся 3 новости
+                    news_hash = __news_parser(message.chat.id, word.lower(), habr_url)
+                    help_text = ''
+                    # Приветсвенное сообщение
+                    bot.send_message(message.chat.id,
+                                     'Во че на районе болтают: ')
+                    # Передаем каждую новость в отдельном сообщении
+                    for item in news_hash:
+                        bot.send_message(message.chat.id,
+                                         item + '\n'+
+                                         news_hash[item])
+                    # Щелкаем флаг
+                    news_flag = False
+                
         else:
             key = 'nothing'
             
@@ -129,9 +158,13 @@ def user_help(message):
     if (weather_flag == True):
             bot.send_message(message.chat.id, config.weather[
                 random.randint(0,
-                               len(config.weather)-1)])
+                              len(config.weather)-1)])
+    
+    # По аналогии с предыдущим
+    if (news_flag == True):
+        bot.send_message(message.chat.id, 'Да за такое в южном бутово по щам дают.')
             
-    sys.stdout.write('{0}\tHashtag:\t{1}\n'.format(message.chat.id, key))
+    sys.stdout.write('{0}\tKey:\t{1}\n'.format(message.chat.id, key))
 
 
 # Парсим погоду на рамблере
@@ -144,9 +177,9 @@ def __weather_parser(chat_id, city='москве'):
     html_doc = (requests.get(rambler_url)).text
     # Парсим
     soup_fd = BeautifulSoup(html_doc, 'html.parser')
-    temperature = soup_fd.find('span', class_='weather-now__value').text
-    description = soup_fd.find('div', class_='weather-today__explanation \
-                                weather-today__grid-explanation').text
+    temperature = (soup_fd.find('span', class_='weather-now__value').text)
+    description = (soup_fd.find('div', class_=
+                                'weather-today__explanation weather-today__grid-explanation').text)
     
     # Выдаем кортеж с распарсенными значениями и удаленными переводами строки
     return ('Ну че\nВ ' + city.replace('\n', '') +
@@ -154,8 +187,36 @@ def __weather_parser(chat_id, city='москве'):
             temperature.replace('\n', '') +
             ' градусов\n' +
             description,
-           'Инфа предоставлена: ' + rambler_url)
+            'Инфа предоставлена: ' + rambler_url)
+
+
+def __news_parser(chat_id, message, url):
+    sys.stdout.write('{0}\tNews url:\t{1}\n'.format(chat_id, url))
+    counter = 0
+    news_hash = {}
+    
+    # Получаем html'ку
+    html_doc = (requests.get(url)).text
+    # Парсим
+    soup_fd = BeautifulSoup(html_doc, 'html.parser')
+    # Ищем все контейнеры с постами на странице
+    post_div = soup_fd.find_all('h2', class_='post__title')
+    
+    # Находим первые 3 поста
+    for counter in range(0,3):
+        post_a_href = post_div[counter].find('a')
+        post_link = post_a_href.get('href')
+        post_title = post_a_href.text
+        news_hash[post_title] = post_link
+
+    # возвращаем словарь с новостями
+    return news_hash
+    
 
 # Все поллим, веб-хуки втопку
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    try:
+        bot.polling(none_stop=True)
+    except Exception as e:
+        sys.stdout.write('{0}\tSomething went wrong with connection:\n{1}\n'.
+                         format('WARNING', e))
--- app/src/config.py
+++ app/src/config.py
@@ -2,8 +2,47 @@
 
 token = ''
 
+ighar_chat_id = ''
+python_ru_chat_id = ''
+
 google_string = 'https://www.google.ru#newwindow=1&q='
 sputnik_string = 'http://www.sputnik.ru/search?q='
+
+habrahabr_rubik =  {'python' : ('python',
+                                'питон',
+                                'пайтон',
+                                'питона',
+                                'питону',
+                                'пайтонам',
+                                'питонам',
+                                'пайтону',
+                                'питонистов',
+                                'питонеров',
+                                'питоне'),
+                    'devops' : ('devops',
+                                'девопс',
+                                'девопса',
+                                'девопсу',
+                                'девопсе',
+                                'девопсеров'),
+                    'go'     : ('go',
+                                'го',
+                                'golang',
+                                'голанг'),
+                    'highload' : ('highload',
+                                  'хайлоад',
+                                  'хайлоаду',
+                                  'хайлоада',
+                                  'хайлоаде'),
+                    'cnotpp'        : ('си',
+                                       'сям')}
+
+habrahabr_url= { 'python' : 'https://habrahabr.ru/hub/python',
+                 'devops' : 'https://habrahabr.ru/hub/devops',
+                 'go' : 'https://habrahabr.ru/hub/go',
+                 'highload' : 'https://habrahabr.ru/hub/hi',
+                 'system_programming' : 'https://habrahabr.ru/hub/system_programming',
+                 'cnotpp' : 'https://habrahabr.ru/hub/c'}
 
 hashtag = {'#убабушки' : ('бабушка',
                           'бабушки',
@@ -19,6 +58,7 @@ hashtag = {'#убабушки' : ('бабушка',
                                'узбекистан'),
            '#ше_не_вмерла' : ('украина',
                               'украине',
+                              'украину',
                               'хохлы',
                               'хохлов',
                               'хохол'),
@@ -46,10 +86,14 @@ weather_hash = {'минске' : 'v-minske/',
                 'учалах' : 'v-uchalakh/',
                 'учалы' : 'v-uchalakh/',
                 'ташкенте' : 'v-tashkente/',
-                'ташкент' : 'v-tashkente/'}
+                'ташкент' : 'v-tashkente/',
+                'краснодар' : 'v-krasnodare',
+                'краснодаре' : 'v-krasnodare'}
 
 time_key = ('время', 'времени', 'часов', 'час')
 
-bad_word_key = ('бот', 'мудак', 'урод', 'параша')
+news_key = ('новости', 'новостям', 'новостей', 'news', 'новостями')
+
+bad_word_key = ('бот')
 
 
